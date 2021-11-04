40e5b2def5da1ccfed5db358d45a5e69d46f14dd
etamar <etamar.levy@gmail.com>
2021-11-04 19:03:04 -0400

    deploy test

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,5 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
 
 
-  # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
