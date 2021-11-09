4d057e7d75aeac31d2ea4a06dad4eb5e4d5bcc53
etamar <etamar.levy@gmail.com>
2021-11-09 01:23:27 -0500

    added icons to dog cards

--- client/src/components/DogCard.js
+++ client/src/components/DogCard.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Card, Button, Container } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDog } from '@fortawesome/free-solid-svg-icons'
 
 function DogCard(props) {
 
@@ -19,14 +21,14 @@ function DogCard(props) {
                 <Card className="text-center dog-card" style={{ width: '18rem' }}>
                     <Card.Img className="dog-img" variant="top" src={props.dog.img_url ? props.dog.img_url : "https://thegraphicsfairy.com/wp-content/uploads/2012/09/Draw-Dogs-Printable-GraphicsFairy21.jpg"} />
                     <Card.Body>
-                        <Card.Title className="dog-name">{props.dog.name}</Card.Title>
-                        <Card.Text>Breed: {props.dog.breed ? props.dog.breed : "Unknown"}</Card.Text>
-                        <Card.Text>Age: {props.dog.age} yo</Card.Text>
-                        <Card.Text>Owner Name: {props.dog.owner_name}</Card.Text>
-                        <Card.Text>Walk date: {props.dog.walk_date}</Card.Text>
-                        <Card.Text>Walk time: {props.dog.walk_time}</Card.Text>
-                        <Card.Text>Phone number: {props.dog.phone_number}</Card.Text>
-                        <Card.Text>Comments: {props.dog.comments ? props.dog.comments : "No comments"}</Card.Text>
+                        <Card.Title className="dog-name"><FontAwesomeIcon icon={faDog} /> {props.dog.name}</Card.Title>
+                        <Card.Text className="dog-info">Breed: {props.dog.breed ? props.dog.breed : "Unknown"}</Card.Text>
+                        <Card.Text className="dog-info">Age: {props.dog.age} yo</Card.Text>
+                        <Card.Text className="dog-info">Owner Name: {props.dog.owner_name}</Card.Text>
+                        <Card.Text className="dog-info">Walk date: {props.dog.walk_date}</Card.Text>
+                        <Card.Text className="dog-info">Walk time: {props.dog.walk_time}</Card.Text>
+                        <Card.Text className="dog-info">Phone number: {props.dog.phone_number}</Card.Text>
+                        <Card.Text className="dog-info">Comments: {props.dog.comments ? props.dog.comments : "No comments"}</Card.Text>
                     </Card.Body>
                     <Card.Body >
                         <Button className="shadow rounded-pill" variant="danger" onClick={deleteDogs}>Delete</Button>
